bb9d26c5c6b0a3882d91bff9f1a16a01176efc44
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-06 16:55:40 +0600

    getting the element name with every click

--- logic.js
+++ logic.js
@@ -1,11 +1,22 @@
 const testId=document.getElementById('test')
+
 const selections = document.querySelectorAll('[data-selection]')
+
+
 
 
 // All Selection laid here above...........
 
 
-printOut(selections)
+selections.forEach(selection=>{
+   selection.addEventListener('click',e=>{
+      const selectionName = selection.dataset.selection
+
+
+      // console.log(selectionName)
+      // printOut(selectionName) 
+   })
+})
 
 
 
