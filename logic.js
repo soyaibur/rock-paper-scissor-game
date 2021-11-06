b0d233716b8b7e04f61ebff1c8f70675d4c01027
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-06 12:43:36 +0600

    CSS completed

---
+++ logic.js
@@ -0,0 +1,12 @@
+const newArray = [133,222,365,412,5322,32126,73212,232,23110]
+
+const b = newArray.find(jakala)
+const f = newArray.findIndex(jakala)
+
+
+function jakala(number){
+   const d = number >= 3232;
+   return d;
+}
+console.log(b)
+console.log(f)
